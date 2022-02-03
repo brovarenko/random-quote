8bbc34dd6ead8e45058b6a582445d5147e9680f0
brovarenko <brovwg@gmail.com>
2022-02-03 21:20:35 +0200

    remove local state

--- src/quotes/quotes.js
+++ src/quotes/quotes.js
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect} from 'react'
 import { useSelector, useDispatch} from 'react-redux'
 import styles from './quotes.module.css';
-import { Transition } from 'react-transition-group';
+
 import { selectQuote, fetchQuotes, selectQuoteStatus } from './quotesSlice'
 
 export const Quote = () => {
     const dispatch = useDispatch()
     const quote = useSelector(selectQuote)
-    const [toggle, setToggle] = useState(false)
+   
     const quoteStatus = useSelector(selectQuoteStatus)
 
     useEffect(() => {
@@ -16,13 +16,9 @@ export const Quote = () => {
         }
       }, [quoteStatus, dispatch])
         
-      return(
-          
+      return( 
           <div className={styles.quoteConteiner}>
-            
-           
-             <div className={`${styles.quote} ${quoteStatus==="loading"?'':styles.animation}`}> {quote.content}</div>
-            
+          <div className={`${styles.quote} ${quoteStatus==="loading"?'':styles.animation}`}> {quote.content}</div>
           <i className={`${styles.author} ${quoteStatus==="loading"?'':styles.animation}`}>-{quote.author}</i>
             
           <button className={styles.btn}
